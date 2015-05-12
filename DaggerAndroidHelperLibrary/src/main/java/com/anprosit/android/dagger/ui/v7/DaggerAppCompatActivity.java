7be825210f1841bac5786ec327d496cba3bb7da1
KeithYokoma <Keith.Yokoma@gmail.com>
2015-05-12 12:53:57 +0900

    Use appcompat

---
+++ DaggerAndroidHelperLibrary/src/main/java/com/anprosit/android/dagger/ui/v7/DaggerAppCompatActivity.java
@@ -0,0 +1,42 @@
+package com.anprosit.android.dagger.ui.v7;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anprosit.android.dagger.DaggerContext;
+import com.anprosit.android.dagger.helper.DaggerHelper;
+
+import java.util.List;
+
+import dagger.ObjectGraph;
+
+/**
+ * @author KeishinYokomaku
+ */
+public abstract class DaggerAppCompatActivity extends AppCompatActivity implements DaggerContext {
+    private DaggerHelper mHelper = new DaggerHelper();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mHelper.onCreate(this, getModules());
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHelper.onDestroy();
+    }
+
+    protected abstract List<Object> getModules();
+
+    @Override
+    public <T> T inject(T obj) {
+        return mHelper.inject(obj);
+    }
+
+    @Override
+    public ObjectGraph getObjectGraph() {
+        return mHelper.getObjectGraph();
+    }
+}
